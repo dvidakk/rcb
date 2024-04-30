c9af4792079b0c89ea408d8a69653bd66b5b1374
dvidak <vidakovicd94@gmail.com>
2024-04-30 21:32:34 +0200

    Refactor user handling logic in main.c and update matrix_client files

--- src/main.c
+++ src/main.c
@@ -2,12 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "matrix/matrix_client.h"
 #include "reddit/reddit.h"
 #include "ini_utils.h"
 
 #include "test.h"
+
+#define USER_TABLE_SIZE 5000
+
+typedef struct {
+    MessageResponse **responses;
+    int size;
+} MessageResponseList;
+
+char* convertTimestampToLocaltime(double timestamp) {
+    time_t time_t_val = (time_t)timestamp;
+    struct tm *time_struct = localtime(&time_t_val);
+    char *time_str = malloc(sizeof(char) * 100);
+    strftime(time_str, 100, "%Y-%m-%d %H:%M:%S", time_struct);
+    return time_str;
+}
+
+void printDevider() {
+    printf("\n");
+    for (int i = 0; i < 100; i++) {
+        printf("-");
+    }
+    printf("\n");
+}
 
 int main() {
 
@@ -27,51 +52,97 @@ int main() {
     Reddit_login(reddit);
     char* redditToken = Reddit_getToken(reddit);
     Reddit_WhoAmI(reddit);
+ 
+    printDevider();
 
     RedMatrix *matrix = RedMatrix_new(redditToken);
     RedMatrix_login(matrix);    
-    MessageResponse *messages = RedMatrix_getRoomMessages(matrix, r_croatia);
 
-    User **users = createUserTable(100);
+    char *from_token = NULL;
+    MessageResponseList *response_list = malloc(sizeof(MessageResponseList));
+    //MessageResponse *messages = RedMatrix_getRoomMessages(matrix, r_croatia);
+
+    response_list->responses = malloc(sizeof(MessageResponse) * USER_TABLE_SIZE);
+    response_list->size = 0;
+
+    int depth = 50;
+    for (int i=0; i < depth; i++) {
+        MessageResponse *messages = RedMatrix_getRoomMessages(matrix, r_croatia, from_token);
+        response_list->responses[response_list->size] = messages;
+        response_list->size++;
+        from_token = messages->end;
+        // sleep for 1 second to avoid rate limiting
+        printf("\r%d/%d", i, depth);
+        fflush(stdout);
+        sleep(1);
+    // print the update with return carriage
+    }
+    User **users = createUserTable(USER_TABLE_SIZE);
     int user_count = 0;
 
-    for (int i = 0; i < messages->state->size; i++) {
-        MessageChunk *message_chunk = messages->state->chunks[i];
-        ChunkContent *content = message_chunk->content;
-        char *displayname = NULL;
+    for (int i = 0; i < response_list->size; i++){
+        MessageResponse *messages = response_list->responses[i];
+        for (int j = 0; j < messages->state->size; j++) {
+            MessageChunk *message_chunk = messages->state->chunks[j];
+            ChunkContent *content = message_chunk->content;
+            char *displayname = NULL;
 
-        if (strcmp(message_chunk->type, "m.room.member") == 0 && getUserDisplayname(users, 100, message_chunk->sender) == NULL) {
-            if(strcmp(content->membership, "join") == 0) {
-                displayname = content->displayname;
-            } else if (strcmp(content->membership, "leave") == 0) {
-                // get the displayname from the previous content in the unsigned chunk
-                displayname = message_chunk->unsigned_chunk->prev_content->displayname;
-            }
+            if (strcmp(message_chunk->type, "m.room.member") == 0 && getUserDisplayname(users, USER_TABLE_SIZE, message_chunk->sender) == NULL) {
+                if(strcmp(content->membership, "join") == 0) {
+                    displayname = content->displayname;
+                } else if (strcmp(content->membership, "leave") == 0) {
+                    // get the displayname from the previous content in the unsigned chunk
+                    if (message_chunk->unsigned_chunk && message_chunk->unsigned_chunk->prev_content) {
+                        displayname = message_chunk->unsigned_chunk->prev_content->displayname;
+                    }
+                }
 
-            if (displayname != NULL) {
-                insertUser(users, 100, message_chunk->sender, displayname);
-                user_count++;
+                if (displayname != NULL) {
+                    insertUser(users, USER_TABLE_SIZE, message_chunk->sender, displayname);
+                    user_count++;
+                }
             }
         }
     }
 
-    for (int i = messages->chunk->size - 1; i >= 0; i--) {
-        MessageChunk *message = messages->chunk->chunks[i];
+for (int i = response_list->size - 1; i >= 0; i--) {
+    MessageResponse *messages = response_list->responses[i];
+    for (int j = messages->chunk->size - 1; j >= 0; j--) {
+        MessageChunk *message = messages->chunk->chunks[j];
         ChunkContent *content = message->content;
-        char* displayname = getUserDisplayname(users, 100, message->sender);
+        char* displayname = getUserDisplayname(users, USER_TABLE_SIZE, message->sender);
 
         if (content->body != NULL) {
-            printf("Message: %s - [%s] %s\n",  content->body, content->msgtype, displayname ? displayname : message->sender);
+
+            if (content->msgtype != NULL && strcmp(content->msgtype, "m.image") == 0) {
+                printf("Image: %s - URL: %s - [%s] %s\n", content->body ? content->body : " ", content->url, content->msgtype, displayname ? displayname : message->sender);
+            } else {
+                printf("Message: %s - [%s] %s\n",  content->body, content->msgtype, displayname ? displayname : message->sender);
+            }
         } else if (strcmp(message->type, "m.room.member") == 0) {
             printf("Member: %s - [%s]\n", strcmp(content->membership, "join") == 0 ? content->displayname : displayname, content->membership);
         } else {
             printf("Message: [Attachment] - [%s] %s\n", message->type, displayname);
         }
     }
+}
+    // print the date range of the messages
+    double start = response_list->responses[0]->chunk->chunks[0]->origin_server_ts / 1000.0;
+    double end = response_list->responses[response_list->size - 1]->chunk->chunks[response_list->responses[response_list->size - 1]->chunk->size - 1]->origin_server_ts / 1000.0;
+
+    printDevider();
+
+    char *start_time_str = convertTimestampToLocaltime(start);
+    printf("Start: %s\n", start_time_str);
+    free(start_time_str);  // Don't forget to free the string when you're done with it
+
+    char *end_time_str = convertTimestampToLocaltime(end);
+    printf("End: %s\n", end_time_str);
+    free(end_time_str);  // Don't forget to free the string when you're done with it
     //RedMatrix_getDisplayName(matrix, "@t2_hltcflqh8:reddit.com");
     //RedMatrix_getJoinedRooms(matrix);
     
-    freeUserTable(users, 100);
+    freeUserTable(users, USER_TABLE_SIZE);
     Reddit_free(reddit);
     RedMatrix_free(matrix);
     return 0;
